a2c105af9bd3656594eec200a99b9c3063fc973e
alanzhangzm <alandevel@outlook.com>
2019-07-29 22:36:55 -0700

    Support for managing multiple servers

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
 <div id="app">
   <router-view
-    :server="manager.servers[manager.serverIndex]"
-    :serverNameList="serverNameList"
-    :isDefault="false"
-    @updateServer="saveData()">
+    :manager="manager"
+    :isDesktop="false">
   </router-view>
 </div>
 </template>
@@ -12,19 +10,7 @@
 <script>
 export default {
   name: 'photon',
-  props: ['manager'],
-  computed: {
-    serverNameList: function () {
-      return this.manager.servers.map(server => {
-        return server.name
-      })
-    }
-  },
-  methods: {
-    saveData: function () {
-      this.manager.writeStorage()
-    }
-  }
+  props: ['manager']
 }
 </script>
 
@@ -32,10 +18,10 @@ export default {
 body {
   margin: 0px;
   background-color: #fff;
-  font-family: "Helvetica Neue", Helvetica, Arial, "PingFang SC", "Hiragino Sans GB", "Heiti SC", "Microsoft YaHei", "WenQuanYi Micro Hei", sans-serif;
+  font-family: "Helvetica Neue", Helvetica, Arial, "Dejavu Sans", "PingFang SC", "Hiragino Sans GB", "Heiti SC", "Microsoft YaHei", "WenQuanYi Micro Hei", "Noto Sans", sans-serif;
 }
 
 input, output, textarea, select, button {
-  font-family: "Helvetica Neue", Helvetica, Arial, "PingFang SC", "Hiragino Sans GB", "Heiti SC", "Microsoft YaHei", "WenQuanYi Micro Hei", sans-serif;
+  font-family: "Helvetica Neue", Helvetica, Arial, "Dejavu Sans", "PingFang SC", "Hiragino Sans GB", "Heiti SC", "Microsoft YaHei", "WenQuanYi Micro Hei", "Noto Sans", sans-serif;
 }
 </style>
